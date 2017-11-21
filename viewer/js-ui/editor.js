cef431387da5517e2c29709182331b809897bc57
Jan Jongboom <janjongboom@gmail.com>
2017-11-21 12:55:00 +0100

    Allow bookmarking of simulator links

--- viewer/js-ui/editor.js
+++ viewer/js-ui/editor.js
@@ -2,9 +2,37 @@ var editor = ace.edit("editor");
 editor.setTheme("ace/theme/textmate");
 editor.getSession().setMode("ace/mode/c_cpp");
 
-document.querySelector('#load-demo').onclick = function() {
-    var sp = document.querySelector('#select-project');
-    var demo = sp.options[sp.selectedIndex].getAttribute('name');
+if (document.location.hash) {
+    if (document.location.hash.indexOf('#user') === 0) {
+        // user script
+        var script = document.location.hash.substr(1);
+        var x = new XMLHttpRequest();
+        x.onload = function() {
+            if (x.status === 200) {
+                editor.setValue(x.responseText);
+                editor.selection.clearSelection();
+                editor.selection.moveCursorTo(0, 0);
+
+                document.querySelector('iframe').src = '/view/' + script;
+            }
+        };
+        x.open('GET', '/out/' + script + '.cpp');
+        x.send();
+    }
+    else {
+        var demo = document.location.hash.substr(1);
+        var ix = [].map.call(document.querySelector('#select-project').options, function(p) {
+            return p.getAttribute('name');
+        }).indexOf(demo);
+
+        if (ix > -1) {
+            load_demo(demo);
+            document.querySelector('#select-project').selectedIndex = ix;
+        }
+    }
+}
+
+function load_demo(demo) {
     var x = new XMLHttpRequest();
     x.onload = function() {
         if (x.status === 200) {
@@ -13,6 +41,7 @@ document.querySelector('#load-demo').onclick = function() {
             editor.selection.moveCursorTo(0, 0);
 
             document.querySelector('iframe').src = '/view/' + demo;
+            document.location.hash = '#' + demo;
         }
         else {
             alert('Failed to compile, see browser console...');
@@ -21,6 +50,12 @@ document.querySelector('#load-demo').onclick = function() {
     };
     x.open('GET', '/demos/' + demo + '/main.cpp');
     x.send();
+}
+
+document.querySelector('#load-demo').onclick = function() {
+    var sp = document.querySelector('#select-project');
+    var demo = sp.options[sp.selectedIndex].getAttribute('name');
+    load_demo(demo);
 };
 
 document.querySelector('#run').onclick = function() {
@@ -37,6 +72,7 @@ document.querySelector('#run').onclick = function() {
         if (x.status === 200) {
             document.querySelector('iframe').src = '/view/' + x.responseText;
             status.textContent = '';
+            document.location.hash = '#' + x.responseText;
         }
         else {
             console.error('Compilation failed', x.status);
