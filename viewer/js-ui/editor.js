434d0ae0e5019458b5c22305bbb987987393e0ea
Jan Jongboom <janjongboom@gmail.com>
2017-12-22 10:55:12 +0000

    Only track on production

--- viewer/js-ui/editor.js
+++ viewer/js-ui/editor.js
@@ -3,7 +3,7 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-3800502-30', { cookieDomain: 'none' });
+ga('create', 'UA-3800502-30');
 ga('send', 'pageview');
 
 var editor = ace.edit("editor");
--- viewer/js-ui/init.js
+++ viewer/js-ui/init.js
@@ -68,5 +68,5 @@ window.MbedJSUI = {};
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-3800502-30', { cookieDomain: 'none' });
+ga('create', 'UA-3800502-30');
 ga('send', 'pageview');
