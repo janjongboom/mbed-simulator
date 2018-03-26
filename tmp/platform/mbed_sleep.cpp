9f06e768d1cd8241747a3bbed55f1b89adc44510
Jan Jongboom <janjongboom@gmail.com>
2018-03-26 17:54:51 +0200

    Remove own copy of Mbed OS

--- tmp/platform/mbed_sleep.cpp
+++
@@ -1,8 +0,0 @@
-#include "mbed_sleep.h"
-
-void sleep_manager_lock_deep_sleep(void) {}
-void sleep_manager_unlock_deep_sleep(void) {}
-bool sleep_manager_can_deep_sleep(void) {
-    return false;
-}
-void sleep_manager_sleep_auto(void) {}
