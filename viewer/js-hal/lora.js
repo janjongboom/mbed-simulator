b624477ae90556f8a5d5c3e43c3d541f8c897058
Jan Jongboom <janjongboom@gmail.com>
2018-12-07 19:20:08 +0800

    Fix RX window timings so LoRaWAN Class C works, and Class A works better

--- viewer/js-hal/lora.js
+++ viewer/js-hal/lora.js
@@ -4,11 +4,38 @@ window.MbedJSHal.lora = (function() {
     var radioPtr = 0;
     var ts = null; // timesync
 
+    // based on https://docs.google.com/spreadsheets/d/1voGAtQAjC1qBmaVuP1ApNKs1ekgUjavHuVQIXyYSvNc/edit#gid=0
+    function calculateTimeOnAir(_dataLength, _datarate, _bandwidth) {
+        var payloadSize = _dataLength;
+        var spreadFactor = _datarate; // already normalized
+        var explicitHeader = 1;
+        var lowDr = 0;
+        var codingRate = 5; // 4/5
+        var preamble = 8;
+        var bandwidth = 125;
+        switch (_bandwidth) {
+            case 7: bandwidth = 125; break;
+            case 8: bandwidth = 250; break;
+            case 9: bandwidth = 500; break;
+        }
+
+        var tsym = Math.pow(2, spreadFactor) / (bandwidth * 1000) * 1000;
+        // console.log('tsym', tsym);
+        var tPreamble = (preamble + 4.25) * tsym;
+        // console.log('tpreamb', tPreamble);
+        var payloadSymNb = 8+(Math.max(Math.ceil((8*payloadSize-4*spreadFactor+28+16-20*(1-explicitHeader))/(4*(spreadFactor-2*lowDr)))*(codingRate),0));
+        // console.log('payloadSymNb', payloadSymNb);
+        var tPayload = payloadSymNb * tsym;
+        var tPacket = tPayload + tPreamble;
+        // console.log('tPacket', tPacket);
+        return tPacket;
+    }
+
     window.socket.on('lora-downlink', function (ev) {
         let buffer = ev.data;
 
         // todo check if its actually msg for us
-        console.log(Date.now(), 'lora-downlink', ev);
+        // console.log(Date.now(), 'lora-downlink', ev);
         obj.downlinkMsg = buffer;
 
         var dataPtr = Module._malloc(buffer.length);
@@ -22,10 +49,12 @@ window.MbedJSHal.lora = (function() {
         }
 
         // when to send?
-        var delay = ev.sendTs - ts.now();
-        console.log('send delay is', delay);
+        var timeOnAir = calculateTimeOnAir(buffer.length, ev.datarate, ev.bandwidth);
+        var delay = ev.sendTs - ts.now() + timeOnAir;
+        // console.log('send delay is', delay);
 
         function go() {
+            console.log(/*Date.now(), */'handle_lora_downlink', 'dataLength', buffer.length, 'freq', ev.freq, 'bandwidth', ev.bandwidth, 'datarate', ev.datarate);
             // @todo: check modulation
             ccall('handle_lora_downlink', null,
                 [ 'number', 'number', 'number', 'number', 'number', 'number' ],
@@ -34,6 +63,8 @@ window.MbedJSHal.lora = (function() {
         }
 
         if (delay <= 0) {
+            // this shouldn't be necessary as it'll be impossible to hit the windows on f.e. Class C, but
+            // need it to work on some slow internet connections
             go();
         }
         else {
@@ -55,26 +86,29 @@ window.MbedJSHal.lora = (function() {
     function sendLoRa(channel, power, bandwidth, datarate, data, size) {
         var buffer = [].slice.call(new Uint8Array(Module.HEAPU8.buffer, data, size));
 
-        var x = new XMLHttpRequest();
-        x.onload = function() {
-            console.log('sendLoRa', x.status, x.responseText);
-        };
-        x.open('POST', host + '/api/lora/send');
-        x.setRequestHeader('Content-Type', 'application/json');
-        x.send(JSON.stringify({
-            payload:
-            buffer,
-            freq: channel,
-            bandwidth: bandwidth,
-            datarate: datarate
-        }));
+        var timeOnAir = calculateTimeOnAir(buffer.length, datarate, bandwidth);
 
-        console.log('sendLoRa', 'channel', channel, 'power', power, 'bandwidth', bandwidth, 'datarate', datarate, 'buffer', buffer, 'size', size);
-        console.log('encoded packet', buffer.map(function(b) {
-            var j = b.toString(16).toUpperCase();
-            if (j.length === 1) return '0' + j;
-            return j;
-        }).join(''));
+        setTimeout(function() {
+            var x = new XMLHttpRequest();
+            x.onload = function() {
+                console.log('sendLoRa', x.status, x.responseText);
+            };
+            x.open('POST', host + '/api/lora/send');
+            x.setRequestHeader('Content-Type', 'application/json');
+            x.send(JSON.stringify({
+                payload: buffer,
+                freq: channel,
+                bandwidth: bandwidth,
+                datarate: datarate
+            }));
+
+            console.log('sendLoRa', 'timeOnAir', timeOnAir, 'channel', channel, 'power', power, 'bandwidth', bandwidth, 'datarate', datarate, 'buffer', buffer, 'size', size);
+            console.log('encoded packet', buffer.map(function(b) {
+                var j = b.toString(16).toUpperCase();
+                if (j.length === 1) return '0' + j;
+                return j;
+            }).join(''));
+        }, timeOnAir);
     }
 
     function sendFsk(channel, power, bandwidth, datarate, data, size) {
