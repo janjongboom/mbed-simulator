2a4680ab6f467712ae62942b35690495b523a798
Jan Jongboom <janjongboom@gmail.com>
2018-12-08 14:18:29 +0800

    Bla|

--- server/launch-server.js
+++ server/launch-server.js
@@ -17,7 +17,7 @@ const version = JSON.parse(fs.readFileSync(Path.join(__dirname, '..', 'package.j
 const compression = require('compression');
 
 const LORA_PORT = 1700;
-const LORA_HOST = '172.20.10.5';
+const LORA_HOST = '192.168.1.62';
 
 let startupTs = Date.now();
 
